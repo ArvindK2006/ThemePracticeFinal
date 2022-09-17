96141fd0b0f39e14af456f94998c22af66a84f53
ArvindK2006 <90778173+ArvindK2006@users.noreply.github.com>
2022-09-17 04:17:00 -0500

    September 17 2022
    Completely revamped, added all components

--- app/src/main/java/com/example/themepractice/MainActivity.java
+++ app/src/main/java/com/example/themepractice/MainActivity.java
@@ -2,15 +2,33 @@ package com.example.themepractice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button confirm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        confirm = (Button) findViewById(R.id.confirmButton);
+
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context context = getApplicationContext();
+                CharSequence text = "Your appointment has been scheduled. Thank you!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
     }
-
-
 }
